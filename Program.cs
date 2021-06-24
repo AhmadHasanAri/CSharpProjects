0821dea4e208e54524e39a539f4d03c8f8bdb966
AhmadHasanAri <86290121+AhmadHasanAri@users.noreply.github.com>
2021-06-24 09:33:08 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace consoleproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter input: ");
+            string greeting = Console.ReadLine();
+            string decoratedGreeting = DecorateString(greeting);
+            Console.WriteLine(decoratedGreeting);
+        }
+        static string DecorateString(string toBeDecorated)
+        {
+            string decoratedString = $"*.*.{toBeDecorated}.*.*";
+            return decoratedString.ToUpper();
+        }
+    }
+}
